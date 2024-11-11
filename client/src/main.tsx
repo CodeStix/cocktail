2efa4f4b6613e66bd3c299e59321cf0a6ad8b708
Stijn Rogiest <reddusted@gmail.com>
2024-11-11 11:24:45 +0100

    Install react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,16 @@ import { Theme } from "@radix-ui/themes";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
 import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Theme accentColor="blue" radius="large">
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" Component={App} />
+                </Routes>
+            </BrowserRouter>
         </Theme>
     </StrictMode>
 );
